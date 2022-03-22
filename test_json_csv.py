8d10284435ead28ec422db597d6692caaf4266c6
Mackenzie Reid <mackenzie.reid@kurtgeiger.com>
2022-03-22 13:20:10 +0000

    Added error handling

--- test_json_csv.py
+++ test_json_csv.py
@@ -1,22 +1,32 @@
 import csv
 import json
+import sys
 
 def csv_to_json(csvFilePath, jsonFilePath):
     jsonArray = []
 
-    with open(csvFilePath, encoding='utf-8') as csvf:
-        csvReader = csv.DictReader(csvf)
-        for row in csvReader:
-            jsonArray.append(row)
-    
-    body = {
-        "key_id": "3",
-        "contacts": jsonArray
-    }
-    body = json.dumps(body, indent=4)
+    try:
+        with open(csvFilePath, encoding='utf-8') as csvf:
+            csvReader = csv.DictReader(csvf)
+            for row in csvReader:
+                jsonArray.append(row)
+    except Exception as e:
+        print >> sys.stderr, "ERROR Error reading csvFile: %s" % csvFilePath
+        
+    try:    
+        body = {
+            "key_id": "3",
+            "contacts": jsonArray
+        }
+        body = json.dumps(body, indent=4)
+    except Exception as e:
+        print >> sys.stderr, "ERROR Error creating json request body"
 
-    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
-        jsonf.write(body)
+    try: 
+        with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+            jsonf.write(body)
+    except Exception as e:
+        print >> sys.stderr, "ERROR Error writing jsonFile: %s" % jsonFilePath
 
 csvFilePath = r'./test_file.csv'
 jsonFilePath = r'./test_file.json'
